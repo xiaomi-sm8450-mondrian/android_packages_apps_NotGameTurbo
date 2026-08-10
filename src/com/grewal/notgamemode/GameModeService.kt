7e6563fec057890d8434e0762545ed8563234c07
keosh <conquer2152016@gmail.com>
2026-08-10 08:18:30 +0000

    NotGameTurbo: Stop polling while the display is off

--- src/com/grewal/notgamemode/GameModeService.kt
+++ src/com/grewal/notgamemode/GameModeService.kt
@@ -7,10 +7,14 @@ package com.grewal.notgamemode
 
 import android.app.Service
 import android.app.usage.UsageStatsManager
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.IBinder
+import android.os.PowerManager
 import android.util.Log
 
 class GameModeService : Service() {
@@ -21,14 +25,29 @@ class GameModeService : Service() {
     private lateinit var handler: Handler
 
     private var lastForeground: String? = null
-    private var gameModeActive = false
     private var activePkg: String? = null
+
+    @Volatile private var gameModeActive = false
+    @Volatile private var polling = false
 
     private val poll =
         object : Runnable {
             override fun run() {
+                if (!polling) {
+                    return
+                }
                 update()
                 handler.postDelayed(this, POLL_INTERVAL_MS)
+            }
+        }
+
+    private val screenReceiver =
+        object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                when (intent.action) {
+                    Intent.ACTION_SCREEN_ON -> startPolling()
+                    Intent.ACTION_SCREEN_OFF -> stopPolling()
+                }
             }
         }
 
@@ -38,20 +57,57 @@ class GameModeService : Service() {
         usageStats = getSystemService(UsageStatsManager::class.java)
         thread = HandlerThread(TAG).apply { start() }
         handler = Handler(thread.looper)
-        handler.post(poll)
+        registerReceiver(
+            screenReceiver,
+            IntentFilter().apply {
+                addAction(Intent.ACTION_SCREEN_ON)
+                addAction(Intent.ACTION_SCREEN_OFF)
+            },
+            Context.RECEIVER_NOT_EXPORTED,
+        )
+        if (getSystemService(PowerManager::class.java).isInteractive) {
+            startPolling()
+        }
         Log.i(TAG, "started")
     }
 
     override fun onDestroy() {
+        unregisterReceiver(screenReceiver)
+        polling = false
         handler.removeCallbacks(poll)
         thread.quitSafely()
-        if (gameModeActive) {
-            TouchFeatureManager.setSuperReport(false)
-            TouchFeatureManager.setGameMode(false)
-            setOrientationTracking(false)
-            gameModeActive = false
-        }
+        disableGameMode()
         super.onDestroy()
+    }
+
+    private fun startPolling() {
+        if (polling) {
+            return
+        }
+        polling = true
+        handler.post(poll)
+        Log.i(TAG, "polling started")
+    }
+
+    private fun stopPolling() {
+        if (!polling) {
+            return
+        }
+        polling = false
+        handler.removeCallbacks(poll)
+        handler.post { disableGameMode() }
+        Log.i(TAG, "polling stopped")
+    }
+
+    private fun disableGameMode() {
+        if (!gameModeActive) {
+            return
+        }
+        gameModeActive = false
+        activePkg = null
+        TouchFeatureManager.setSuperReport(false)
+        TouchFeatureManager.setGameMode(false)
+        setOrientationTracking(false)
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
@@ -81,12 +137,8 @@ class GameModeService : Service() {
                 applyTuning(foreground!!)
                 TouchFeatureManager.setSuperReport(prefs.isSuperReport(foreground!!))
             }
-        } else if (gameModeActive) {
-            gameModeActive = false
-            activePkg = null
-            TouchFeatureManager.setSuperReport(false)
-            TouchFeatureManager.setGameMode(false)
-            setOrientationTracking(false)
+        } else {
+            disableGameMode()
         }
     }
 
